a43cc6fccb30f7ad18f8f2cd23d2f01cf3a93fea
DesignPro23 <spiderman@8thday.net>
2026-07-19 03:11:00 -0400

    - delete wrangler.jsonc - remove support for dynamic site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,4 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import cloudflare from '@astrojs/cloudflare';
-
-export default defineConfig({
-  adapter: cloudflare()
-});
+export default defineConfig({});
