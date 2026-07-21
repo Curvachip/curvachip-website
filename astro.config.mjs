91cfd94e0a36ecd05559603598f974d2616902f0
DesignPro23 <spiderman@8thday.net>
2026-07-21 19:18:19 -0400

    - remove .md config and calls - remove url redirects in favor of cloudflare redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,24 +7,5 @@ export default defineConfig({
   site: "https://curvachip.com",
   trailingSlash: "never",
 
-  redirects: {
-      "/about-us": "/about",
-      "/shadow-task": "/apps/shadow-task",
-      "/tipzap": "/apps/tipzap",
-      "/stoneclip-rock-identifier": "/apps/stoneclip",
-      "/grocery-wiz": "/apps/grocery-wiz",
-
-      "/privacy-policy-shadow-task": "/privacy/shadow-task",
-      "/privacy-policy-tipzap": "/privacy/tipzap",
-      "/privacy-policy-stoneclip": "/privacy/stoneclip",
-      "/privacy-policy-bugclip": "/privacy/bugclip",
-      "/privacy-policy-plantclip": "/privacy/plantclip",
-      "/privacy-policy-grocery-liz": "/privacy/grocery-wiz",
-      "/privacy-policy-tally-snap": "/privacy/tally-snap",
-
-      "/privacy-policy-curvachip": "/privacy/website",
-      "/terms-and-conditions-curvachip": "/privacy/terms-of-use",
-  },
-
   integrations: [sitemap()],
 });
