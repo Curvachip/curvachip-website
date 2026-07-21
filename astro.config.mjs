f2050379d80ae32f7cf0f06b93929b3c99751b01
DesignPro23 <spiderman@8thday.net>
2026-07-21 14:50:12 -0400

    - add sitemap to faciliate ai agent resources

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,29 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import sitemap from "@astrojs/sitemap";
+
 export default defineConfig({
-    trailingSlash: "never",
-    redirects: {
-        "/about-us": "/about",
-        "/shadow-task": "/apps/shadow-task",
-        "/tipzap": "/apps/tipzap",
-        "/stoneclip-rock-identifier": "/apps/stoneclip",
-        "/grocery-wiz": "/apps/grocery-wiz",
+  trailingSlash: "never",
 
-        "/privacy-policy-shadow-task": "/privacy/shadow-task",
-        "/privacy-policy-tipzap": "/privacy/tipzap",
-        "/privacy-policy-stoneclip": "/privacy/stoneclip",
-        "/privacy-policy-bugclip": "/privacy/bugclip",
-        "/privacy-policy-plantclip": "/privacy/plantclip",
-        "/privacy-policy-grocery-liz": "/privacy/grocery-wiz",
-        "/privacy-policy-tally-snap": "/privacy/tally-snap",
+  redirects: {
+      "/about-us": "/about",
+      "/shadow-task": "/apps/shadow-task",
+      "/tipzap": "/apps/tipzap",
+      "/stoneclip-rock-identifier": "/apps/stoneclip",
+      "/grocery-wiz": "/apps/grocery-wiz",
 
-        "/privacy-policy-curvachip": "/privacy/website",
-        "/terms-and-conditions-curvachip": "/privacy/terms-of-use",
-    },
+      "/privacy-policy-shadow-task": "/privacy/shadow-task",
+      "/privacy-policy-tipzap": "/privacy/tipzap",
+      "/privacy-policy-stoneclip": "/privacy/stoneclip",
+      "/privacy-policy-bugclip": "/privacy/bugclip",
+      "/privacy-policy-plantclip": "/privacy/plantclip",
+      "/privacy-policy-grocery-liz": "/privacy/grocery-wiz",
+      "/privacy-policy-tally-snap": "/privacy/tally-snap",
+
+      "/privacy-policy-curvachip": "/privacy/website",
+      "/terms-and-conditions-curvachip": "/privacy/terms-of-use",
+  },
+
+  integrations: [sitemap()],
 });
